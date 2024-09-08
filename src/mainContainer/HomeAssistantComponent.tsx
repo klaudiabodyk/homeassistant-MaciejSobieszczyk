38e259e07e10eb797d5b0f8e0d412ac507921242
Klaudia <klaudia.bodyk@icloud.com>
2024-09-08 17:16:55 +0200

    Add console logs & add header

--- src/mainContainer/HomeAssistantComponent.tsx
+++ src/mainContainer/HomeAssistantComponent.tsx
@@ -22,17 +22,26 @@ const HomeAssistantComponent = () => {
     const [states, setStates] = useState<Entity[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    console.log('states', states)
+    console.log('error', error)
+    console.log('loading', loading)
+
 
     useEffect(() => {
         fetchStates();
     }, []);
 
+    const API_BASE_URL = 'http://ha.mshomedomain.com/api';
+
+    const stateUrl = `${API_BASE_URL}`;
+    console.log(stateUrl);
     const fetchStates = async () => {
         try {
-            const response = await axios.get('http://localhost:8123/api/states', {
+            const response = await axios.get(stateUrl, {
                 headers: {
                     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNDRlMWNkYjRmY2Y0ODAzYmNiMjM4ZTMxNWEwNzIwNyIsImlhdCI6MTcyNTgwNjM1OSwiZXhwIjoyMDQxMTY2MzU5fQ.gTJEZ_1uVYftGxTX4Knk4YXKoUDfYuLuKdyoJpxcHcE',
                     'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
                 },
             });
             setStates(response.data);
@@ -46,26 +55,7 @@ const HomeAssistantComponent = () => {
 
     return (
         <div>
-            {loading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>Error: {error}</p>
-            ) : (
-                <ul>
-                    {states.map((state, index) => (
-                        <li key={index}>
-                            <strong>{state.entity_id}:</strong> {state.state}
-                            {state.attributes && Object.keys(state.attributes).length > 0 && (
-                                <span> Attributes: {JSON.stringify(state.attributes)}</span>
-                            )}
-                            <br />
-                            Last Changed: {state.last_changed}
-                            <br />
-                            Last Updated: {state.last_updated}
-                        </li>
-                    ))}
-                </ul>
-            )}
+            Hello
         </div>
     );
 };
