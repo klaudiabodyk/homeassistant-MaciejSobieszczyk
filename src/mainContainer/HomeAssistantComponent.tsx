422d348ca1e6e6d75f04f9af5f3140a4b7b1a60b
Klaudia <klaudia.bodyk@icloud.com>
2024-09-21 09:48:21 +0200

    Add menu & static image

--- src/mainContainer/HomeAssistantComponent.tsx
+++ src/mainContainer/HomeAssistantComponent.tsx
@@ -1,61 +1,68 @@
-// src/components/HomeAssistantComponent.tsx
-
-import { useState, useEffect } from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import axios from 'axios';
+import Menu from "./components/menu/Menu.tsx";
+import _ from "lodash";
+import {requiredHeaders, stateUrl} from "./config/config.ts";
+import image from "../assets/Warstwa_2.png";
+import "./HomeAssistant.css";
 
-interface State {
-    state: string;
-    attributes?: Record<string, unknown>;
-    last_changed: string;
-    last_updated: string;
-}
-
-interface Entity {
-    entity_id: string;
-    state: string;
-    attributes?: Record<string, unknown>;
-    last_changed: string;
-    last_updated: string;
-}
 
 const HomeAssistantComponent = () => {
-    const [states, setStates] = useState<Entity[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-    console.log('states', states)
-    console.log('error', error)
-    console.log('loading', loading)
-
+    const [stateData, setStateData] = useState<Record<string, any>>({});
+    const [isLight, setIsLight] = useState<boolean>(false);
 
     useEffect(() => {
         fetchStates();
     }, []);
 
-    const API_BASE_URL = 'http://ha.mshomedomain.com/api';
-
-    const stateUrl = `${API_BASE_URL}`;
-    console.log(stateUrl);
     const fetchStates = async () => {
         try {
             const response = await axios.get(stateUrl, {
-                headers: {
-                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNDRlMWNkYjRmY2Y0ODAzYmNiMjM4ZTMxNWEwNzIwNyIsImlhdCI6MTcyNTgwNjM1OSwiZXhwIjoyMDQxMTY2MzU5fQ.gTJEZ_1uVYftGxTX4Knk4YXKoUDfYuLuKdyoJpxcHcE',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
+                headers: requiredHeaders,
             });
-            setStates(response.data);
-            setLoading(false);
+
+            console.log('API response:', response.data);
+            setStateData(response.data);
         } catch (err) {
             console.error('Error fetching states:', err);
-            setError('Failed to fetch states');
-            setLoading(false);
         }
     };
 
+    const getRgbColor = (data?: Record<string, any>): number[] | undefined => {
+        if (!data || typeof data !== 'object') {
+            return undefined;
+        }
+
+        return data.attributes?.rgb_color;
+    };
+
+    const rgbColor = useMemo(() => {
+        const isLightThere = getRgbColor(stateData)
+        if (isLightThere === undefined ) {
+            setIsLight(false);
+            return console.warn('Swiatlo wylaczone')
+        }
+        setIsLight(true)
+        return isLightThere;
+    }, [stateData]);
+
+    const hexColor = rgbColor ? rgbColor.map(val => val.toString(16).padStart(2, '0')).join('') : '';
+    console.log(rgbColor);
+    console.log(hexColor);
+
     return (
-        <div>
-            Hello
+        <div >
+            {isLight && !_.isEmpty(hexColor) ? ( <input
+                type="color"
+                id="favcolor"
+                name="favcolor"
+                value={`#${hexColor}`}
+            />): <h3>Uwaga! Światło w pokoju jest wyłączone</h3>}
+            <div className={'main-home'}>
+                <Menu />
+                <img src={image} alt={'bob'} className={'image-size'} />
+            </div>
+
         </div>
     );
 };
---
+++ src/mainContainer/components/menu/Menu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="wrapper">
+            <div className="menuContainer">
+                <h1 className="header">Home 1</h1>
+                <button className="button">Room 1</button>
+                <button className="button">Room 2</button>
+                <button className="button">Room 3</button>
+                <button className="button">Room 4</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
